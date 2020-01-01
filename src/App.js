5e6a00e8e84ec6ae08ac4410714c01ce746aab0f
Sergey Pushkovskiy <push_s@mail.ua>
2020-01-01 09:45:20 +0200

    Build frontend

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/places/new" component={NewPlace} />
-        <Route exact path="/" component={Users} />
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/places/new" component={NewPlace} />
+          <Route path="/:userId/places" component={UserPlaces} />
+          <Route exact path="/" component={Users} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
--- src/user/components/UsersList.jsx
+++ src/user/components/UsersList.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+
 import "./UsersList.css";
 
 const UsersList = props => {
   if (props.items.length === 0) {
-    return <h2 className="center">No users found</h2>;
+    return (
+      <Card>
+        <h2 className="center">No users found</h2>
+      </Card>
+    );
   }
   return (
     <ul className="users-list">
