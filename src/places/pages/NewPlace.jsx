0efc6736ef7449ab03409b82b1525a7fe25573b5
sp <sp@Sergey-MacBook-Pro.local>
2020-01-09 22:17:15 +0200

    Started working with Inputs

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
 
+import "./NewPlace.css";
+
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[]}
+        onInput={"adad"}
+      />
     </form>
   );
 };
