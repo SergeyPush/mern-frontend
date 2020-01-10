bd2a02846f86b066c2329edfc27a2bd9920903d7
Sergey Pushkovskiy <onyxpsi@ukr.net>
2020-01-10 18:56:30 +0200

    Work with new place

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Input from "../../shared/components/FormElements/Input";
-
+import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
   return (
     <form className="place-form">
       <Input
         type="text"
         label="Title"
         element="input"
-        validators={[]}
-        onInput={"adad"}
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+        errorText="please enter a valid title"
       />
     </form>
   );
